dd9f6a4963f646d4f76aa5ab078a7e2374959cc8
Nicoló Vergaro <nikovergaro@gmail.com>
2022-11-23 18:09:19 +0100

    Create NLP_dataset.py

---
+++ NLP_dataset.py
@@ -0,0 +1,54 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from nltk.tokenize import sent_tokenize
+import json
+class NLP_dataset(Dataset):
+    def __init__(self, json_file,model_type="gpt2", max_length=1024, add_abstract=True,add_highlights=True, is_test=True):
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_type)
+        self.papers = []
+        self.labels = []
+        self.is_test = is_test
+        end_token = "<|endoftext|>"
+        start_token = "<|startoftext|>"
+        sep = " "
+        if not add_abstract and not add_highlights:
+            print("Must contain at least one betwen highlights and abstract!")
+            return
+        with open(json_file) as f:
+            data = json.load(f)
+            
+        
+        #create a string that contains highlights (optional) and abstract (optional)
+        for paper in data.values():
+            s = start_token
+
+            s += " ".join(paper["highlights"]) if add_highlights else ""
+            if add_abstract:
+                if add_highlights:
+                    # add separator between highlights and abstract
+                    s+= sep
+                abstract = paper['abstract']
+                if len(s) + len(abstract) < max_length:
+                    s+=paper['abstract']
+                else:
+                    #tokenize and add sentences until we reach a length of 1024
+                    tokens = sent_tokenize(abstract)
+                    for token in tokens:
+                        if len(s) + len(token) <= max_length - len(end_token):
+                            s += token
+                        else:
+                            break
+                
+            s += end_token
+            self.papers.append(s)
+            if self.is_test:
+                self.labels.append(paper['title'])
+            
+    
+    def __len__(self):
+        return len(self.papers)
+    
+    def __getitem__(self,item):
+        if not self.is_test:
+            return self.papers[item], self.labels[item]
+        else:
+            return self.papers[item]
