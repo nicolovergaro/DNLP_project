88e489dd301767f33a24c6d8b39e6d7d4d273843
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2022-11-24 15:09:26 +0100

    Update NLP_dataset.py

--- utils/NLP_dataset.py
+++ utils/NLP_dataset.py
@@ -1,60 +1,80 @@
 import json
 from nltk.tokenize import sent_tokenize
 from torch.utils.data import Dataset
-from transformers import GPT2Tokenizer
 
-
-class NLP_dataset(Dataset):
-    def __init__(self, json_file, model_name="gpt2", max_length=1024, add_abstract=True, add_highlights=True, is_test=True):
-        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+class EncodedDataset(Dataset):
+    def __init__(self, json_file, tokenizer, max_length=1024, include_highlights=True, include_abstract=True, is_test=True):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
         self.is_test = is_test
-        self.papers = []
-        self.labels = []
         
-        end_token = "<|endoftext|>"
-        start_token = "<|startoftext|>"
+        self.x = []
+        self.y = []
+        
         sep = " "
         
-        # check that at least some data will be put in the strings
-        if not add_abstract and not add_highlights:
-            print("Must contain at least one betwen highlights and abstract!")
+        # check that at least come data will be added to the content
+        if not include_highlights and not include_abstract:
+            print("!!!!! WARNING: must contein at least one between highlights and abstract. !!!!!")
             return
         
-        # read the data from the json file
+        # read the file with data
         with open(json_file) as f:
             data = json.load(f)
-        
-        # create a string that contains highlights (optional) and abstract (optional)
+            
         for paper in data.values():
-            s = start_token
-
-            s += " ".join(paper["highlights"]) if add_highlights else ""
-            if add_abstract:
-                if add_highlights:
-                    # add separator between highlights and abstract
+            s = tokenizer.bos_token
+            
+            s += sep.join(paper["highlights"]) if include_highlights else ""
+            
+            if include_abstract:
+                if include_highlights:  # add separator between highlights and abstract
                     s += sep
-                abstract = paper['abstract']
-                if len(s) + len(abstract) < max_length:
-                    s+=paper['abstract']
-                else:
-                    # tokenize and add sentences until we reach a length of 1024
-                    for token in sent_tokenize(abstract):
-                        if len(s) + len(token) <= max_length - len(end_token):
+                    
+                # check if the entire abstract fits in the length limit of the model
+                if len(s) + len(paper["abstract"]) <= max_length:
+                    s += paper["abstract"]
+                else:  # if not add sentence by sentence until reaching the length limit
+                    for token in sent_tokenize(paper["abstract"]):
+                        if len(s) + len(token) <= max_length:
                             s += token
                         else:
                             break
                             
-            s += end_token
-            self.papers.append(s)
-            if self.is_test:
-                self.labels.append(paper['title'])
+            s += tokenizer.eos_token
+                            
+            self.x.append(s)
+            self.y.append(paper["title"])
+            
+    def __getitem__(self, i):
+        ins = self.tokenizer.encode_plus(self.x[i],
+                                    padding="max_length",
+                                    max_length=self.max_length,
+                                    truncation=True,
+                                    return_attention_mask=True,
+                                    return_tensors='pt',
+                                    return_token_type_ids=True)
+        input_ids = ins["input_ids"]
+        attention_mask = ins["attention_mask"]
+        token_type_ids = ins["token_type_ids"]
+        
+        outs = ins = self.tokenizer.encode_plus(self.x[i],
+                                    self.y[i],
+                                    padding="max_length",
+                                    max_length=self.max_length,
+                                    truncation=True,
+                                    return_attention_mask=True,
+                                    return_tensors='pt',
+                                    return_token_type_ids=True)
+        output_ids = outs["input_ids"]
+        
+        if self.is_test is None:
+            return input_ids[0], attention_mask[0], token_type_ids[0]
+        else:
+            return input_ids[0], attention_mask[0], token_type_ids[0], output_ids[0]
     
     def __len__(self):
-        return len(self.papers)
+        return len(self.x)
     
-    def __getitem__(self,item):
-        if self.is_test:  # test
-            return self.papers[item]
-        else:
-            return self.papers[item], self.labels[item]
-            
+    def _gettexts_(self, i):
+        return self.x[i], self.y[i]
