1e870e39e2414ecc9035022e6324d79080e1b5c4
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2023-02-14 10:14:06 +0100

    Update reproducibility.py

--- utils/reproducibility.py
+++ utils/reproducibility.py
@@ -11,7 +11,6 @@ def make_it_reproducible(seed=SEED):
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
-    torch.manual_seed(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
